94970e08b288267ab239c8c34a342692170dc326
oozankerem <ozann.keremm1@gmail.com>
2026-05-31 07:27:51 +0300

    proje

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Ozan Kerem
+# 2420171039
+# Sertifika Bağlantıları:
+# https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=lK1hwq46DD
+# https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=0Koh8emYP1
+# 5eab3179-c5bb-4af0-80de-a18d5f78114d
+
+date -Iseconds > report.log
+
+wmic cpu get name >> report.log
+wmic memorychip get capacity >> report.log
+wmic baseboard get product,Manufacturer >> report.log
+wmic csproduct get uuid >> report.log
+getmac >> report.log
+
+echo "Lütfen parolayı giriniz:"
+read -s PAROLA
+
+gpg --batch --yes --passphrase "$PAROLA" --symmetric --cipher-algo AES256 -o report.log.gpg report.log
+
+rm report.log
